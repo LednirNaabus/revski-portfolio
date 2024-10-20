9d4876d070531c28c2bf97944db1c9190cca0998
Rindel Subaan <valsubaan@yahoo.com>
2024-10-20 20:00:00 +0800

    Download PDF file feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
         'simple-icons': ['*'],
           'skill-icons': ['*'],
           'logos': ['*'],
+          'fluent-mdl2': ['*'],
       },
     }),
     playformCompress({
